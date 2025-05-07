fd581368200b85fd22ecfa2411ea76ae544209bf
raj chavan <chavanraj1010@gmail.com>
2025-05-07 10:17:13 +0530

    add authentication  middleware

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    
+
+    const path = request.nextUrl.pathname
+
+    const isPublicPath = path === '/'
+
+
+    //get token from local storage 
+    const token = request.cookies.get('authToken')?.value;
+    
+
+    // if(isPublicPath && token ){
+    //     return NextResponse.redirect(new URL('/home', request.url))
+    // }
+
+    if(!isPublicPath && !token){
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+ 
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher:[
+    '/',
+    '/home',
+    '/dashboard'
+  ],
+
+}
