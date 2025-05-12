a3053cb32edf3fc6c4b331db36f2ec3fa3da0fd1
rushikeshs102 <suryawanshir102@gmail.com>
2025-05-12 17:59:27 +0530

    api modified for ValidateEntGateLoginApplicationToken

--- src/app/api/AppValidation/ValidateEntGateLoginApplicationToken/route.ts
+++ src/app/api/AppValidation/ValidateEntGateLoginApplicationToken/route.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
 
-    try{
+    try {
         const body = await request.json();
         const UserID = request.cookies.get('userId')?.value;
-        const {ApplicationToken, AppId, RoleName, DistributorCode, WWID } = await body;
+        const { ApplicationToken, AppId, RoleName, DistributorCode, WWID } = await body;
 
         const payload = {
             ApplicationToken: ApplicationToken,
@@ -17,24 +17,38 @@ export async function POST(request: NextRequest) {
             WWID: WWID
         };
 
-        const res = await axios.post("https://testapp1.rediport.in/api/UserMaster/ValidateEntGateLoginApplicationToken", payload)
+        const encodedToken = encodeURIComponent(ApplicationToken);
+
+        console.log("Encoded token", encodedToken);
+        
+        const refererUrl = `"https://testapp1.rediport.in/entgatelogin?code=${encodedToken}&userId=3&appId=1&roleName=J%26J+COE&distributorCode=&wwid=20240430`;
+        console.log("Referer URL", refererUrl);
+
+
+        const res = await axios.post("https://testapp1.rediport.in/api/UserMaster/ValidateEntGateLoginApplicationToken", payload,
+            {
+                headers: {
+                    Referer: refererUrl
+                }
+            }
+        )
         // console.log(res.data);
         if (!res.data) {
             throw new Error("Failed to fetch project data");
         }
         // console.log("Response token", res.data.token);
 
-        
-        const reportToken:string = res.data.token;
+
+        const reportToken: string = res.data.token;
 
         const responseData = NextResponse.json(
-            { message: "Success",status:200 }
-          );
+            { message: "Success", status: 200 }
+        );
 
         responseData.cookies.set('reportToken', reportToken, { httpOnly: true });
         return responseData;
 
-    }catch (error) {
+    } catch (error) {
         console.error("Error in ValidateEntGateLoginApplicationToken:", error);
         return NextResponse.json(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
     }
